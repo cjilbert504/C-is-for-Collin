3285e10bd921b7a6f9cb279aaff116a5f23a4dbe
Collin Jilbert <cjilbert504@gmail.com>
2023-06-03 11:13:18 -0500

    learning about defining and assigning to variables

---
+++ defining-variables.c
@@ -0,0 +1,36 @@
+main()
+{
+  // My variables for the program
+  // Defining / Declaring Variables
+
+  char answer;
+  int quantity;
+  float price;
+
+  // Rest of program would follow
+  
+  /* You can define more than one variable of the same type on one line. So
+     this:
+
+  char first_initial;
+  char middle_initial;
+
+  Can actually be written as:
+
+  char first_initial, middle_initial;
+  */
+
+  // Assigning Values to Variables
+  answer = 'B';
+  quantity = 14;
+  price = 7.95;
+
+  // You can also store answers to expressions in variables:
+  // price = 8.50 * .65;
+  
+  /* In the above example price was defined to store floats and since the
+     result of the expression is itself a float, this will work */
+
+  /* You can user other variables in the expression:
+     totalAmount = price * quantity; */
+}
