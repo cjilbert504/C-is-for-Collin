05b992e8e96c80e3be5bdd6b8b93d6bbf21047aa
Collin Jilbert <cjilbert504@gmail.com>
2023-06-03 08:23:53 -0500

    initial commit

---
+++ ch4-ex1.c
@@ -0,0 +1,15 @@
+// Absolute Beginner's Guide to C, 3rd Edition
+// Chapter 4 Example 1--Chatper4ex1.c
+
+#include <stdio.h>
+
+int main()
+{
+  /* These three lines show you how to use the most popular Escape Sequences */
+  printf("Column A\tColumn B\tColumn C");
+  printf("\nMy Computer\'s Beep Sounds Like This: \a!\n");
+  printf("\"Letz\bs fix that typo and then show the backslash ");
+  printf("character \\\" she said\n");
+
+  return 0;
+}
