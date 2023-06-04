cec0ad8dcda383fcd507d6299e44a0c2c4c12a42
Collin Jilbert <cjilbert504@gmail.com>
2023-06-04 14:09:28 -0500

    add Ruby version

---
+++ ch4/ex2.rb
@@ -0,0 +1,12 @@
+puts "Quantity\tCost\tTotal"
+printf("%d\t\t$%.2f\t$%.2f\n", 3, 9.99, 29.97)
+print "Too many spaces    \b\b\b\b can be fixed with the "
+puts "\\b Escape character"
+print "\n\a\n\a\n\a\n\aSkip a few lines, and beep "
+puts "a few beeps."
+printf("\n\n%s %c.", "You are kicking butt learning", 'C')
+printf("You just finished chapter %d.\nYou have finished ", 4)
+printf("%.1f%c of the book.\n", 12.500, '%')
+printf("\n\nOne third equals %.2f or ", 0.333333);
+printf("%.3f or %.4f or ", 0.333333, 0.333333);
+printf("%.5f or %.6f\n\n\n", 0.333333, 0.333333);
