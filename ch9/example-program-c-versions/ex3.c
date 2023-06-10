f31df04f38782586659f9433d613447466596ec5
Collin Jilbert <cjilbert504@gmail.com>
2023-06-09 21:54:31 -0500

    example using operators, mulitple assignment, and other bits

---
+++ ch9/example-program-c-versions/ex3.c
@@ -0,0 +1,37 @@
+// Example program #3 from Chapter9 of Absolute Beginner's Guide to
+// C, 3rd Edition
+// File Chapter9ex3.c
+
+/* This program calculates the average of four grades and also does
+   some other basic math. */
+
+#include <stdio.h>
+
+int main()
+{
+  int grade1, grade2, grade3, grade4;
+  float averageGrade, gradeDelta, percentDiff;
+
+  /* The student got 88s on the first and third test,
+     so a multiple assignment works. */
+  grade1 = grade3 = 88;
+  grade2 = 79;
+
+  // The user needs to enter the fourth grade
+  printf("What did you get on the fourth test");
+  printf(" (An integer between 0 and 100)?");
+  scanf(" %d", &grade4);
+
+  averageGrade = (grade1 + grade2 + grade3 + grade4) / 4;
+
+  printf("Your average is %.1f.\n", averageGrade);
+
+  gradeDelta = 95 - averageGrade;
+  percentDiff = 100 * ((95 - averageGrade) / 95); // Don't know why the authors didn't just do: percentDiff = 100 * (gradeDelta / 95); instead here.
+  printf("Your grade is %.1f points lower than the ", gradeDelta);
+  printf("top grade in the class (95)\n");
+  printf("You are %.1f percent behind ", percentDiff);
+  printf("that grade!\n\n\n");
+
+  return 0;
+}
